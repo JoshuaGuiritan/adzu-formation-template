448c4674cb647f425ad226bf9239b69d57f07252
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-08-25 04:21:14 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function App() {
 
   const [sidebar, setSideBar] = useState(false);
-  const [table, setTable] = useState(false);
+  const [home, setHome] = useState(false);
+  const [records, setRecords] = useState(false);
+  const [accinfo, setAccInfo] = useState(false);
 
   const hoverSideBar = () => {
     setSideBar(sidebar => sidebar ? false : true);
   }
 
+  const hoverHome = () => {
+    setHome(sidebar => sidebar ? false : true);
+  }
+
+  const hoverRecords = () => {
+    setRecords(sidebar => sidebar ? false : true);
+  }
+
+  const hoverAccInfo = () => {
+    setAccInfo(sidebar => sidebar ? false : true);
+  }
+
   return (
       <div className="d-flex flex-column vh-100">
         <div className="row g-0">
-          <div className={`${sidebar ? "col-1" : "col-2"} sidebar-animation bg-darkblue text-white py-3 fs-4 fw-bold d-flex justify-content-center align-items-center`}>
+          <div className={`${sidebar ? "col-1 custom-half-width" : "col-2"} sidebar-animation bg-darkblue text-white py-3 fs-4 fw-bold d-flex justify-content-center align-items-center`}>
             { sidebar ? <FontAwesomeIcon icon="fa-solid fa-graduation-cap" className="fs-4"/> : <img src="./Images/adzu-header-final.png" style={{height: '24px'}}/>}
           </div>
-          <div className={`${sidebar ? "col-11" : "col-10"} sidebar-animation bg-blue d-flex justify-content-between align-items-center`}>
+          <div className={`${sidebar ? "col-11 fill-remaining-space" : "col-10"} sidebar-animation bg-blue d-flex justify-content-between align-items-center`}>
             <a className="bar-hover h-100 px-3 d-flex justify-content-between align-items-center cursor-pointer" role="button" onClick={hoverSideBar}>
               <FontAwesomeIcon icon="fa-solid fa-bars" className="text-white1 fs-6" />
             </a>
@@ -31,43 +45,92 @@ function App() {
             </div>
           </div>
         </div>
+        
         <div className="row flex-grow-1 g-0">
-          <div className={`${sidebar ? "col-1" : "col-2"} sidebar-animation bg-gray2 text-white2 d-flex flex-column justify-content-start`}>
-            {/* <div className="position-relative p-3">
-                <input
-                    className="form-control small py-2 ps-2 pe-5 bg-gray1 border-0 rounded-1 text-white searchbar-placeholder" // Added text-white for visibility
-                    type="text"
-                    placeholder="Search menu..."
-                />
-                <div className="position-absolute top-50 end-0 translate-middle-y me-4">
-                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" className="text-yellowgray small" />
-                </div>
-            </div> */}
+          <div className={`${sidebar ? "col-1 custom-half-width" : "col-2"} sidebar-animation bg-gray2 text-white2 d-flex flex-column justify-content-start ${sidebar && "align-items-center"}`}>
+            {!sidebar && (
+              <>
             <div className="bg-gray3 p-2">
               <span className="text-lightgray small fw-light"><small><small>MAIN NAVIGATION</small></small></span>
             </div>
-            <div class="accordion accordion-flush" id="accordionFlushExample">
+            <a className={`ps-1 text-decoration-none navigation-hover`} type="button">
+              <button class="accordion-button py-2 bg-gray2 gap-2" type="button">
+                <FontAwesomeIcon icon="fa-solid fa-house" className="text-yellow fs-6 ms-3" />
+                <span class="small text-light"><small>Home</small></span>
+              </button>
+            </a>
+            <a class={`accordion accordion-flush`} id="accordionFlushExample" type="button">
               <div class="accordion-item">
-                <h2 class="accordion-header">
-                  <button class="accordion-button collapsed py-3 bg-gray2 d-flex justify-content-between gap-2" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <div class="accordion-header">
+                  <button class={`accordion-button collapsed py-2 bg-gray2 gap-2 navigation-hover`} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                     <FontAwesomeIcon icon="fa-solid fa-table" className="text-yellow fs-6"/>
                     <span class="small text-light"><small>Records</small></span>
                   </button>
-                </h2>
-                <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                  <div class="accordion-body py-2 fs-6 bg-gray1 text-lightgray">
-                    Placeholder content for this accordion, which is intended to demonstrate the class. This is the first item’s accordion body.
+                </div>
+                <div id="flush-collapseOne" className="accordion-collapse collapse bg-gray1" data-bs-parent="#accordionFlushExample">
+                    <div className="d-flex flex-column justify-content-start text-lightgray">
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>ADMIN</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>FFP</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>CON</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>CSITE</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>SED</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>SMA</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>CS</small></span>
+                      </a>
+                      <a className="py-1 text-decoration-none recordlist-hover text-lightgray d-flex align-items-center" type="button">
+                        <small><FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" className="small ms-4" /></small>
+                        <span class="small ms-2"><small>PPO</small></span>
+                      </a>
                   </div>
                 </div>
               </div>
-            </div>
-            
+              
+              
+            </a>
+            <div className="bg-gray3 p-2">
+                <span className="text-lightgray small fw-light"><small><small>SETTINGS</small></small></span>
+              </div>
+              <a className={`ps-1 text-decoration-none navigation-hover`} type="button">
+                <button class="accordion-button py-2 bg-gray2 gap-2" type="button">
+                  <FontAwesomeIcon icon="fa-solid fa-sliders" className="text-yellow fs-6 ms-3"/>
+                  <span class="small text-light"><small>Account Information</small></span>
+                </button>
+              </a>
+            </>
+            )}
+            {sidebar && (
+              <>
+              <FontAwesomeIcon icon="fa-solid fa-house" className="text-yellow fs-5 mt-3" />
+              <FontAwesomeIcon icon="fa-solid fa-table" className="text-yellow fs-5 mt-3"/>
+              <FontAwesomeIcon icon="fa-solid fa-sliders" className="text-yellow fs-5 mt-3"/>
+              </>
+            )}
           </div>
-          <div className={`${sidebar ? "col-11" : "col-10"} sidebar-animation bg-white2`}>
+          <div className={`${sidebar ? "col-11 fill-remaining-space" : "col-10"} sidebar-animation bg-white2`}>
           </div>
         </div>
       </div>
   )
 }
 
-export default App
+export default App;
